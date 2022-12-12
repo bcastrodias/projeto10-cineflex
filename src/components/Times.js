583476017944aded86c9078f61fa0735b6eba410
Bruno <b.castro.doute@gmail.com>
2022-12-12 13:21:28 -0300

    horarios

--- src/components/Times.js
+++ src/components/Times.js
@@ -1,1 +1,36 @@
-    
+import Axios from "axios";
+import { useState, useEffect } from "react";
+
+const Times = (props) => {
+  const APItimes = `https://mock-api.driven.com.br/api/v8/cineflex/movies/${props.id}/showtimes`;
+  const [days, setDays] = useState();
+
+  useEffect(() => {
+    Axios.get(APItimes).then((result) => {
+      console.log(result);
+      setDays(result.data.days);
+    });
+  }, []);
+
+  return (
+    <>
+      {days?.map((day) => {
+        return (
+          <>
+            <div>
+              {day.weekday} - {day.date}
+            </div>
+            {day.showtimes.map((showtime) => {
+              const onPressSession = () => {
+                setSelectedSession(showtime.id);
+              };
+              return <button onClick={onPressSession}>{showtime.name}</button>;
+            })}
+          </>
+        );
+      })}
+    </>
+  );
+};
+
+export default Times;
